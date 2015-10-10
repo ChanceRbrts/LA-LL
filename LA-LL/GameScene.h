c18cc291550084fff7a8872dbc69376ea1370025
PlatPlat48 <roberts.chance@gmail.com>
2015-10-10 09:26:32 -0600

    Initial Commit

---
+++ LA-LL/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  LA-LL
+//
+
+//  Copyright (c) 2015 Chance Roberts. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ LA-LL/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LA-LL
+//
+//  Created by Chance Roberts on 10/10/15.
+//  Copyright © 2015 Chance Roberts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
